98db4bb30d2eef277a6b4322635a0b4f6ed04a72
Ryan <159220794+Sandoop@users.noreply.github.com>
2024-11-06 03:44:37 +0000

    signup and login stuff

--- simple_login.py
+++ simple_login.py
@@ -1,1 +1,26 @@
+#import require module hashlib
+import hashlib
 
+def signup():
+# User sign up function
+    email = input("Enter your email address: ")
+    pwd = input("Enter your password: ")
+    confpwd = input("Re-enter your password: ")
+    if pwd == confpwd:
+        enc = pwd.encode()
+        hash1 = hashlib.md5(enc).hexdigest()
+        with open("credentials.txt", "w") as f:
+            f.write(email + "\n")
+            f.write(hash1)
+        print("Password matches")
+    else:
+        print("Password does not match\n")
+
+def login():
+#User login function
+    email = input("Enter Email ")
+    pwd = ("Enter password ")
+    auth = pwd.encode()
+    auth_hash = hashlib.md5(auth).hexdigest()
+    with open("credentials.txt", "r") as f:
+        stored_email, stored_pwd = f.read().split("\n")
